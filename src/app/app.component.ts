b45cacc2213c385dc2de075bb7aa6eff6b3e955c
MaruAguilera <mari.ceci.agui@gmail.com>
2021-01-21 21:18:23 -0300

    Angular Store

---
+++ src/app/app.component.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+import { Product } from './product.model';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'platzi-store';
+
+  items = ['Marisa', 'Romina', 'Chris'];
+
+  products: Product [] = [
+    {
+      id: '1',
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/images/buzo.jpg',
+      title: 'Buzo',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/images/mug.jpg',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/images/pin1.jpg',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'assets/images/sticker1.jpg',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      image: 'assets/images/sticker.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    }
+  ];
+
+  addItem(){
+    this.items.push('nuevo item');
+  }
+  deleteItem(index: number){
+    this.items.splice(index, 1);
+  }
+
+  clickProduct(id:number){
+    console.log('product');
+    console.log(id);
+  }
+
+}
